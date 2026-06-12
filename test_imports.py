932884fbb9807853311d2ab2d3eabf22aba052a1
anbu789 <anbu919155@gmail.com>
2026-06-12 11:09:18 +0530

    Environment and env created

--- test_imports.py
+++
@@ -1,8 +0,0 @@
-# add this to test_imports.py
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("Gemini key loaded:", bool(os.getenv("GOOGLE_API_KEY")))
-print("HuggingFace token loaded:", bool(os.getenv("HUGGINGFACE_TOKEN")))
